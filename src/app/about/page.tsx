3de8ecadbb87b2608705eb2bac45dcf466d37247
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-01-11 20:48:24 +0000

    Dynamic rendering

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,10 @@
-export default function AboutPage() {
+import { cookies } from 'next/headers';
+export default async function AboutPage() {
  console.log('About server component ');
- return <h1>About page</h1>;
+ const cookieStore = cookies();
+ const theme = (await cookieStore).get('theme');
+ console.log('theme', theme);
+ return <h1>About page {new Date().toLocaleTimeString()}</h1>;
 }
+
+//next.js switches to dynamic rendering automatically when encounters functions in the component like cookies(), headers(), or searchParams object
