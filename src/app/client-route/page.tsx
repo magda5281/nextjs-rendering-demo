ef7a1442be48531fce3818bddd3e5337817e43fc
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-01-12 10:18:00 +0000

    Updated client-route

--- src/app/client-route/page.tsx
+++ src/app/client-route/page.tsx
@@ -1,12 +1,15 @@
 'use client';
-import { serverSideFunction } from '@/app/utils/server-utils';
+
+//Error:   × You're importing a component that needs "server-only". That only works in a Server Component but one of its parents is marked with "use client", so it's a Client Component.
+// │ Learn more: https://nextjs.org/docs/app/building-your-application/rendering
+// import { serverSideFunction } from '@/app/utils/server-utils';
 export default function ClientRoutePage() {
  console.log('Client route rendered ');
- const result = serverSideFunction();
+ //  const result = serverSideFunction();
  return (
   <>
    <h1>Client route page</h1>
-   <p>{result}</p>
+   {/* <p>{result}</p> */}
   </>
  );
 }
