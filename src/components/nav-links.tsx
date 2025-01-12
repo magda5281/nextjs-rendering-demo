a2a7c7050688f24ae9e677560c86190f826d49f6
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-01-12 12:21:07 +0000

    Place client component as low as possible in the page tree

---
+++ src/components/nav-links.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default function NavLinks() {
+ console.log('Nav links rendered ');
+ return <div>List of nav links </div>;
+}
---
+++ src/components/nav-search.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import { useState } from 'react';
+
+export default function NavSearch() {
+ console.log('Nav search rendered');
+ const [search, setSearch] = useState();
+ return <div>Nav search input </div>;
+}
