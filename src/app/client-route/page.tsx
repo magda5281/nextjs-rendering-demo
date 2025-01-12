b6f7956c2aedff6fa7915f787819d24cba143d26
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-01-12 11:35:06 +0000

    Use client-only
    Client only function used in server route will cause an error

--- src/app/client-route/page.tsx
+++ src/app/client-route/page.tsx
@@ -1,35 +1,16 @@
 'use client';
 
-import React from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import { clientSideFunction } from '@/app/utils/client-utils';
+
 import { useTheme } from '@/components/ThemeProvider';
 
 export default function ClientRoutePage() {
  console.log('Client route rendered ');
  const theme = useTheme();
- const settings = {
-  dots: true,
- };
-
+ const result = clientSideFunction();
  return (
   <div className='image-slider-container'>
-   <h1 style={{ color: theme.colors.primary }}>Client route </h1>
-   <Slider {...settings}>
-    <div>
-     <img src='http://picsum.photos/400/200' />
-    </div>
-    <div>
-     <img src='http://picsum.photos/400/200' />
-    </div>
-    <div>
-     <img src='http://picsum.photos/400/200' />
-    </div>
-    <div>
-     <img src='http://picsum.photos/400/200' />
-    </div>
-   </Slider>
+   <h1 style={{ color: theme.colors.primary }}>Client route {result}</h1>
   </div>
  );
 }
---
+++ src/app/utils/client-utils.ts
@@ -0,0 +1,5 @@
+import 'client-only';
+export const clientSideFunction = () => {
+ console.log('use window object', 'use Local storage');
+ return 'client result';
+};
