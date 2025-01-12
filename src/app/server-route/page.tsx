35dd9bc5b9797cf97f26be433304ad28b254e900
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-01-12 10:34:32 +0000

    Create ImageSlider component and use in server-route

--- src/app/server-route/page.tsx
+++ src/app/server-route/page.tsx
@@ -1,37 +1,14 @@
-// import { serverSideFunction } from '@/app/utils/server-utils';
-import React from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import { serverSideFunction } from '@/app/utils/server-utils';
+import ImageSlider from '@/components/ImageSlider';
+
 export default function ServerRoutePage() {
  console.log('Server route rendered ');
- //  const result = serverSideFunction();
- const settings = {
-  dots: true,
- };
+ const result = serverSideFunction();
  return (
   <>
    <h1>Server route page</h1>
-   {/* <p>{result}</p> */}
-
-   <div className='image-slider-container'>
-    <Slider {...settings}>
-     <div>
-      <img src='http://picsum.photos/400/200' />
-     </div>
-     <div>
-      <img src='http://picsum.photos/400/200' />
-     </div>
-     <div>
-      <img src='http://picsum.photos/400/200' />
-     </div>
-     <div>
-      <img src='http://picsum.photos/400/200' />
-     </div>
-    </Slider>
-   </div>
+   <p>{result}</p>
+   <ImageSlider />
   </>
  );
 }
-
-//adding component that uses client server features in server component will cause error
